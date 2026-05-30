d2bcc7c9deb78025fd75e3faab35ada13961fbc7
Yazdan <mmlyfmmlyfm@gmail.com>
2026-05-30 13:50:48 +0330

    add hello

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+	<p style={{color: "red", fontSize: "24px"}}>سلام دنیا!</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
